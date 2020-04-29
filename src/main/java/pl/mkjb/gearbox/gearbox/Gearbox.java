3375a80c453ed59caa7bcd09e78a7544b383f02b
kmucha555 <Cfki83vm7>
2020-04-29 22:50:10 +0200

    Adding fields and methods to Gearbox.

--- src/main/java/pl/mkjb/gearbox/gearbox/Gearbox.java
+++ src/main/java/pl/mkjb/gearbox/gearbox/Gearbox.java
@@ -1,9 +1,23 @@
 package pl.mkjb.gearbox.gearbox;
 
 import lombok.RequiredArgsConstructor;
+import pl.mkjb.gearbox.gearbox.shared.Gear;
 import pl.mkjb.gearbox.gearbox.shared.GearLimit;
 
 @RequiredArgsConstructor
 class Gearbox {
     private final GearLimit gearLimit;
+    private int currentGear;
+
+    public void changeGear(Gear gear) {
+        this.currentGear = gear.getGear();
+    }
+
+    public Gear getCurrentGear() {
+        return new Gear(currentGear);
+    }
+
+    public GearLimit getGearLimit() {
+        return gearLimit;
+    }
 }
---
+++ src/main/java/pl/mkjb/gearbox/gearbox/shared/Gear.java
@@ -0,0 +1,10 @@
+package pl.mkjb.gearbox.gearbox.shared;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Gear {
+    private final int gear;
+}
